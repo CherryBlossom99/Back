d6f0a27b83124d9595666d95b1212706459d7da3
Taeyeon Park <106387717+CherryBlossom99@users.noreply.github.com>
2023-04-01 16:53:52 +0900

    sample 2023 04 01

---
+++ walkoo/src/main/java/com/walkoo/walkoo/WalkooApplication.java
@@ -0,0 +1,13 @@
+package com.walkoo.walkoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkooApplication.class, args);
+	}
+
+}
